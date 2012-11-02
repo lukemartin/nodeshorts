fe125caf6bed727092365c0c5b0d95a94241124d
Luke Martin <luke@lukemartin.me>
2012-11-02 08:43:11 +0000

    model checking & adding

--- app.js
+++ app.js
@@ -27,7 +27,13 @@
 	var Schema = mongoose.Schema;
 
 	var Short = new Schema({
-		url:          { type: String, required: true }	
+		url: { type: String, required: true },
+		slug: { type: String, required: true},
+		visits: { type: Number, default: 0 },
+		unique_visits: { type: Number, default: 0 },
+		ips: { type: Array },
+		created: { type: Date, default: Date.now },
+		modified: { type: Date, default: Date.now }
 	});
 
 	var ShortModel = mongoose.model('Short', Short);
@@ -40,22 +46,49 @@
 	app.post('/api/shorts', function (req, res) {
 		var short;
 
-		console.log(req.body);
-
-		short = new ShortModel({
+		ShortModel.findOne({
 			url: req.body.url
-		});
-
-		short.save(function (error) {
+		}, function (error, short) {
 			if (!error) {
-				return console.log('created record', short);
+				if (!short) {
+					short = new ShortModel({
+						url: req.body.url,
+						slug: hasher(req.body.url)
+					});
+
+					short.save(function (error) {
+						if (!error) {
+							return console.log('created record', short);
+						} else {
+							return console.log(error);
+						}
+					});
+
+					return res.send(short);
+				} else {
+					return res.send('exists');
+				}
+				return;
 			} else {
 				return console.log(error);
 			}
 		});
-
-		return res.end(short);
 	});
+
+	var hasher = function (URL, length) {
+		if (!length) {
+			length = 6;
+		}
+
+		var AUID = [],
+			CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+		
+		for (var i = 0; i < length; i = i + 1) {
+			AUID.push(CHARS[Math.floor(Math.random() * 62)]);
+		}
+		
+		return AUID.join('');
+	};
 
 	app.listen(port);
 	console.log('app running on port ', port);
